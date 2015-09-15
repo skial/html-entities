66e353e30660158045ce734f4790b8a0b9cb8b24
Skial Bainn <skial.bainn@gmail.com>
2015-09-15 13:52:59 +0100

    Add new template template/HtmlEntities.hx which provides two arrays, one containing all the entity names, the other all entity codepoints. Length and index should match.

---
+++ template/HtmlEntities.hx
@@ -0,0 +1,17 @@
+package uhx.sys;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+class HtmlEntities {
+
+	public static var names:Array<String> = [
+		$names
+	];
+	
+	public static var values:Array<Array<Int>> = [
+		$values
+	];
+	
+}
