5f19e41605138ebd84bf8f0837080678b79efe64
Skial Bainn <skial.bainn@gmail.com>
2015-09-16 11:06:16 +0100

    Turn template/HtmlEntity.hx into abstract of `String`. Remove Helper class and other inline methods. Simplify `toHtmlEntity` method.

--- template/HtmlEntity.hx
+++ template/HtmlEntity.hx
@@ -9,30 +9,10 @@ import unifill.InternalEncoding;
  * @author Skial Bainn
  * Names and codepoint values built from https://html.spec.whatwg.org/multipage/entities.json
  */
-@:forward(iterator) @:enum abstract HtmlEntity(Array<Int>) from Array<Int> to Array<Int> {
-	
-	@:from public static inline function fromCodePoint(codepoint:CodePoint):Array<Int> {
-		return [codepoint.toInt()];
-	}
-	
-	@:from public static inline function fromCodePoints(codepoints:Array<CodePoint>):Array<Int> {
-		return [for (codepoint in codepoints) codepoint.toInt()];
-	}
-	
-	@:from public static inline function fromString(value:String):Array<Int> {
-		return Helper.fromHtml( value );
-	}
-	
-	@:to public inline function toCodePoints():Array<CodePoint> {
-		return [for (i in this) CodePoint.fromInt(i)];
-	}
-	
-	@:to public inline function toString():String {
-		return InternalEncoding.fromCodePoints( this );
-	}
+@:forward @:enum abstract HtmlEntity(String) from String to String {
 	
 	public inline function toHtmlEntity():String {
-		return Helper.toHtml( this );
+		return '&$this;';
 	}
 	
 	/**
@@ -41,22 +21,4 @@ import unifill.InternalEncoding;
 	 */
 	
 	$values
-}
-
-private class Helper {
-	
-	public static function fromHtml(value:String):Array<Int> {
-		return switch(value) {
-			$fromCases
-			case _: [for (codepoint in new CodePointIter( value )) codepoint.toInt()];
-		}
-	}
-	
-	public static function toHtml(codepoints:Array<Int>):String {
-		return switch (codepoints) {
-			$toCases
-			case _: InternalEncoding.fromCodePoints( codepoints );
-		}
-	}
-	
 }
