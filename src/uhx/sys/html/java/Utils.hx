21a32f295694d9006dfdeb4e73f1626d245a4ecf
Brendon <skial.bainn@gmail.com>
2019-08-19 13:42:10 +0100

    Minor README.md update.

--- src/uhx/sys/html/java/Utils.hx
+++ src/uhx/sys/html/java/Utils.hx
@@ -8,10 +8,6 @@ import haxe.macro.Context;
 class Utils {
 
     public static macro function propertiesLoader():Expr {
-        if (Context.defined('debug')) {
-            trace(Context.resolvePath('uhx/sys/html/java/HtmlEntity.properties'));
-            
-        }
         Context.addResource(
             'HtmlEntity.properties', 
             sys.io.File.getBytes(Context.resolvePath('uhx/sys/html/java/HtmlEntity.properties'))
