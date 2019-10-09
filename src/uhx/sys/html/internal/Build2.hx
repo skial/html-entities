e8ca2dfe29435b0ef5ef438ed8cc153bdf8f67db
Brendon <skial.bainn@gmail.com>
2019-10-09 14:39:58 +0100

    Attempt to fix whitespace

--- src/uhx/sys/html/internal/Build2.hx
+++ src/uhx/sys/html/internal/Build2.hx
@@ -13,37 +13,37 @@ using haxe.io.Path;
 using sys.FileSystem;
 
 enum abstract Defines(String) {
-	public var DryRun = 'dryrun';
-	public var Save = 'save';
+    public var DryRun = 'dryrun';
+    public var Save = 'save';
 
-	@:to public inline function defined():Bool {
-		return haxe.macro.Context.defined(this);
-	}
+    @:to public inline function defined():Bool {
+        return haxe.macro.Context.defined(this);
+    }
 }
 
 typedef JsonData = {
-	var codepoints:Array<Int>;
-	var characters:String;
+    var codepoints:Array<Int>;
+    var characters:String;
 }
 
 typedef TemplateCtx = JsonData & {
-	var ident:String;
-	var value:String;
+    var ident:String;
+    var value:String;
 }
 
 class Build2 {
-	
-	private static var keywords = ['in' => 'In'];
+    
+    private static var keywords = ['in' => 'In'];
     private static var printer = new haxe.macro.Printer();
-	public static function main() extract();
-	public static function extract() {
-		var cwd = Sys.getCwd();
-		var json = '$cwd/resources/entities.json'.normalize();
-		
-		if (json.exists()) {
-			var data:DynamicAccess<JsonData> = Json.parse( json.getContent() );
-			var names:Array<String> = [];
-			var map:Map<String, Array<Int>> = [];
+    public static function main() extract();
+    public static function extract() {
+        var cwd = Sys.getCwd();
+        var json = '$cwd/resources/entities.json'.normalize();
+        
+        if (json.exists()) {
+            var data:DynamicAccess<JsonData> = Json.parse( json.getContent() );
+            var names:Array<String> = [];
+            var map:Map<String, Array<Int>> = [];
 
             for (key => value in data) {
                 var _key = key.substring(1, key.length - 1);
@@ -82,8 +82,8 @@ class Build2 {
                 Context.fatalError('startingPoints chars and index are not equal lengths. Generation Failer.', Context.currentPos());
             }
 
-			var mph = new hash.Mph();
-			var table = mph.make(map);
+            var mph = new hash.Mph();
+            var table = mph.make(map);
 
             var tmp = macro class HtmlEntityImpl {
                 private static var mph:hash.Mph = new hash.Mph();
@@ -127,26 +127,26 @@ class Build2 {
                 Handle java/jvm specific files.
             **/
             var javaProperties = '$cwd/template/JavaTpl.properties'.normalize();
-		    var javaTpl = '$cwd/template/JavaAbstract.hx'.normalize();
+            var javaTpl = '$cwd/template/JavaAbstract.hx'.normalize();
             var fields:Array<TemplateCtx> = [for (name in names) {
-				var value = '&${name};';
-				var info = data.get(value);
-				if (keywords.exists(name)) name = keywords.get(name);
+                var value = '&${name};';
+                var info = data.get(value);
+                if (keywords.exists(name)) name = keywords.get(name);
 
-				// There appears to be an issue printing `info.characters` in eval mode.
-				{
-					ident: name,
-					value: value,
-					codepoints: info.codepoints,
-					characters: info.characters,
-				};
-			}];
+                // There appears to be an issue printing `info.characters` in eval mode.
+                {
+                    ident: name,
+                    value: value,
+                    codepoints: info.codepoints,
+                    characters: info.characters,
+                };
+            }];
 
-			if (DryRun) {
+            if (DryRun) {
                 trace( printer.printTypeDefinition(tmp) );
 
-			} else if (Save) {
-				var buffer = new StringBuf();
+            } else if (Save) {
+                var buffer = new StringBuf();
                 buffer.add( "package uhx.sys.html.std;" );
                 buffer.add( '\n' );
                 buffer.add( 'using StringTools;' );
@@ -176,7 +176,7 @@ class Build2 {
                 var abs = javaProperties.getContent();
                 var tpl = new Template(abs);
                 var javaProps = tpl.execute({
-				    fields:fields
+                    fields:fields
                         .map( f -> {ident:f.ident, value:f.value, characters:f.characters, codepoints:f.codepoints.join(',')}), 
                     typeName:'HtmlEntity'
                 });
@@ -184,49 +184,49 @@ class Build2 {
                 var tpl = new Template(abs);
                 var javaAbs = tpl.execute({fields:fields, typeName:'HtmlEntity'});
                 var javaout = '$cwd/src/uhx/sys/html/java/HtmlEntity.hx'.normalize();
-				var javaprops = '$cwd/src/uhx/sys/html/java/HtmlEntity.properties'.normalize();
+                var javaprops = '$cwd/src/uhx/sys/html/java/HtmlEntity.properties'.normalize();
 
                 javaout.saveContent( javaAbs );
-				javaprops.saveContent( javaProps );
-			}
-			
-		}
+                javaprops.saveContent( javaProps );
+            }
+            
+        }
 
-	}
+    }
 
-	private static function alphaSort(a:String, b:String) {
+    private static function alphaSort(a:String, b:String) {
         var aCode = a.charCodeAt(0);
         var bCode = b.charCodeAt(0);
         
-		if (aCode >= 'a'.code && aCode <= 'z'.code) {
-			aCode -= 32;
-		} else if (aCode >= 'A'.code && aCode <= 'Z'.code) {
-			aCode += 32;
-		}
-		if (bCode >= 'a'.code && bCode <= 'z'.code) {
-			bCode -= 32;
-		} else if (bCode >= 'A'.code && bCode <= 'Z'.code) {
-			bCode += 32;
-		}
+        if (aCode >= 'a'.code && aCode <= 'z'.code) {
+            aCode -= 32;
+        } else if (aCode >= 'A'.code && aCode <= 'Z'.code) {
+            aCode += 32;
+        }
+        if (bCode >= 'a'.code && bCode <= 'z'.code) {
+            bCode -= 32;
+        } else if (bCode >= 'A'.code && bCode <= 'Z'.code) {
+            bCode += 32;
+        }
 
         return if (aCode == bCode) {
-			if (a.length > b.length) {
-				1;
+            if (a.length > b.length) {
+                1;
 
-			} else if (a.length < b.length) {
-				-1;
+            } else if (a.length < b.length) {
+                -1;
 
-			} else if (a.length > 1 && b.length > 1) {
-				var _a = totalValue(a);
-				var _b = totalValue(b);
+            } else if (a.length > 1 && b.length > 1) {
+                var _a = totalValue(a);
+                var _b = totalValue(b);
 
-				if (_a == _b) {
-					0;
-				} else if (_a > _b) {
-					1;
-				} else {
-					-1;
-				}
+                if (_a == _b) {
+                    0;
+                } else if (_a > _b) {
+                    1;
+                } else {
+                    -1;
+                }
                 
             } else {
                 0;
@@ -243,18 +243,18 @@ class Build2 {
 
     }
 
-	private static function totalValue(value:String):Int {
-		return value
-			.split('')
-			.map( s -> s.charCodeAt(0) )
-			.map( i -> if (i >= 'a'.code && i <= 'z'.code) {
-				i-= 32;
-			} else if (i >= 'A'.code && i <= 'Z'.code) {
-				i+= 32;
-			} else {
-				i;
-			} )
-			.fold( (v, c)-> c+=v, 0 );
-	}
-	
+    private static function totalValue(value:String):Int {
+        return value
+            .split('')
+            .map( s -> s.charCodeAt(0) )
+            .map( i -> if (i >= 'a'.code && i <= 'z'.code) {
+                i-= 32;
+            } else if (i >= 'A'.code && i <= 'Z'.code) {
+                i+= 32;
+            } else {
+                i;
+            } )
+            .fold( (v, c)-> c+=v, 0 );
+    }
+    
 }
